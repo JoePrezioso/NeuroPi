4ffb9ab9a468cf0eb6f45b84b2246cd04ca792c2
Valerie Magri <3lucky10@gmail.com>
2014-05-02 15:38:12 -0400

    made gui, started integration

--- neuropi_game.py
+++ neuropi_game.py
@@ -1,5 +1,6 @@
 import pygame
-from python-mindwave-mobile import MindwaveDataPointReader
+import random
+from python_mindwave_mobile.MindwaveDataPointReader import MindwaveDataPointReader
 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
@@ -10,12 +11,29 @@ done = False
 
 clock = pygame.time.Clock()
 
+#if __name__ == '__main__':
+#    mindwaveDataPointReader = MindwaveDataPointReader()
+#    mindwaveDataPointReader.start()
+meditation = 50
+attention = 50
+poor_signal = 50
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     screen.fill((120,120,120))
+
+    #dataPoint = mindwaveDataPointReader.readNextDataPoint()
+    #if (not dataPoint.__class__ is RawDataPoint):
+    #    print dataPoint
+    meditation+=random.choice([1,0,-1])
+    attention+=random.choice([1,0,-1])
+    poor_signal+=random.choice([1,0,-1])
+
+    screen.fill((100,200,200),(100,200-meditation*2,100,meditation*2))
+    screen.fill((200,200,100),(300,200-attention*2,100,attention*2))
+    screen.fill((200,100,100),(500,200-poor_signal,100,poor_signal))
     pygame.display.flip()
 
     clock.tick(60)
