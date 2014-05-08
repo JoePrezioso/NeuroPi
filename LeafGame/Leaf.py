b665eb88d94bc74d52fc32ff2953dc03b768cb5e
ArcticSphinx <jxp8159@rit.edu>
2014-05-08 13:33:48 -0400

    Fixed indentation. Found new problem with pygame.init().

--- LeafGame/Leaf.py
+++ LeafGame/Leaf.py
@@ -1,8 +1,6 @@
-import random, os
-import pygame
-#Need this to use the mindwave
-from python_mindwave_mobile.MindwaveDataPointReader import MindwaveDataPointReader
-from pygame.locals import *
+import random, os, pygame
+#Need line below to use the mindwave -- commented out for use on other machines
+#from python_mindwave_mobile.MindwaveDataPointReader import MindwaveDataPointReader
 
 #initialize font module
 pygame.init()
@@ -41,11 +39,7 @@ poor_signal = 50
 def load_image(file_name):
 	full_name = os.path.join('assets', file_name)
 	
-	try:
-		image = pygame.image.load(full_name)
-	except pygema.error, message:
-		print ("This image broke: " + full_name)
-		raise SystemExit, message
+	image = pygame.image.load(full_name)
 	
 	image = image.convert()
 	
@@ -61,47 +55,48 @@ def titleScreen():
    
 #Same error every time: IndentationError: unindent does not match any outer indentation level
 #There shouldn't be any reason for me to get this error...
+#'''
 def gameLoop():
     #run until done is set to true
-    while not done:
+	while not done:
 
-        for event in pygame.event.get():
-            #break out of the loop if 'A' is pressed
-            if event.key == K_a:
-                done = True
+		for event in pygame.event.get():
+			#break out of the loop if 'A' is pressed
+			if event.key == K_a:
+				done = True
+	
+	screen.fill((120,120,120))
 
-        screen.fill((120,120,120))
+	#dataPoint = mindwaveDataPointReader.readNextDataPoint()
+	#if (not dataPoint.__class__ is RawDataPoint):
+	#    print dataPoint
+	meditation+=random.choice([1,0,-1])
+	attention+=random.choice([1,0,-1])
+	poor_signal+=random.choice([1,0,-1])
 
-        #dataPoint = mindwaveDataPointReader.readNextDataPoint()
-        #if (not dataPoint.__class__ is RawDataPoint):
-        #    print dataPoint
-        meditation+=random.choice([1,0,-1])
-        attention+=random.choice([1,0,-1])
-        poor_signal+=random.choice([1,0,-1])
+	#leaf lift and leaf acceleration 
+	leafLift = meditation*2
+	leafAccel = attention*2
 
-        #leaf lift and leaf acceleration 
-        leafLift = meditation*2
-        leafAccel = attention*2
+	screen.fill((100,200,200),(100,200-leafLift,100,meditation*2))
+	screen.fill((200,200,100),(300,200-leafAccel,100,attention*2))
+	screen.fill((200,100,100),(500,200-poor_signal,100,poor_signal))
+	pygame.display.flip()
 
-        screen.fill((100,200,200),(100,200-leafLift,100,meditation*2))
-        screen.fill((200,200,100),(300,200-leafAccel,100,attention*2))
-        screen.fill((200,100,100),(500,200-poor_signal,100,poor_signal))
-        pygame.display.flip()
+	#speed (210 to keep you from stopping the leaf) reduced by acceleration 
+	leafSpeed = 210 - leafAccel
+	#height increased by meditation
+	leafHeight = 0 + leafLift
 
-        #speed (210 to keep you from stopping the leaf) reduced by acceleration 
-        leafSpeed = 210 - leafAccel
-        #height increased by meditation
-        leafHeight = 0 + leafLift
-
-        #add number of milliseconds since last call to score
-        score += clock.get_time()
-        #Note: Font rendering is not thread safe!
-        #(text, anti-alias (boolean), RGB val for color, background=None)
-        pygame.font.Font.render(score, True, (0,255,0))
-        #clock ticks
-        clock.tick(60)
-    pass
-    
+	#add number of milliseconds since last call to score
+	score += clock.get_time()
+	#Note: Font rendering is not thread safe!
+	#(text, anti-alias (boolean), RGB val for color, background=None)
+	pygame.font.Font.render(score, True, (0,255,0))
+	#clock ticks
+	clock.tick(60)
+		
+#'''
 def main():
 	#We will generate objects randomly. So, we must seed.
 	random.seed()
@@ -110,8 +105,7 @@ def main():
     
     
     #run the gameLoop
-    gameLoop()
-
+    #gameLoop()
    
 #Call main()
 main()
