2ef9ba87e52a0034ddf1b92af59fcbe9c5c2797f
Valerie Magri <3lucky10@gmail.com>
2014-05-22 23:50:57 -0400

    added debug variable and apples; close #35, close #32

--- LeafConnected.py
+++ LeafConnected.py
@@ -2,6 +2,7 @@ import random, os, pygame, threading
 from python_mindwave_mobile.MindwaveDataPointReader import MindwaveDataPointReader
 from pygame.locals import *
 from datathread import GetData
+from objects import Item
 
 #initialize font module
 pygame.init()
@@ -12,19 +13,11 @@ winX = 700
 global winY 
 winY = 500
 
-#Velocity
-global xVelocity
-xVelocity = 5
-global yVelocity
-yVelocity = 5
-
-#x and y variables for leaf
-global leafX
-leafX = 20
-global leafY
-leafY = 20
-
 size = (winX,winY)
+
+#debug variable
+headset = False
+
 
 random.seed(100)
 
@@ -42,13 +35,14 @@ clock = pygame.time.Clock()
 #start the reader
 if __name__ == '__main__':
     mindwaveDataPointReader = MindwaveDataPointReader()
-    mindwaveDataPointReader.start('74:E5:43:89:54:2E')
+    if headset:
+        mindwaveDataPointReader.start('74:E5:43:89:54:2E')
 
 #file name, x value, and y value for image to be loaded
-def loadImage(fileName, xVal, yVal):
+def loadImage(fileName):
     img = pygame.image.load(os.path.join('LeafGame', 'assets', fileName))
     img = img.convert_alpha()
-    screen.blit(img, (xVal,yVal))
+    return img
  
 #title screen
 def titleScreen():
@@ -75,7 +69,8 @@ def titleScreen():
 #code to spawn an apple
 def spawnApple():
     yRand = random.randrange(0, (winY - 60))
-    loadImage('Apple.png', (winX - 60), yRand)
+    apple = Item(loadImage('Apple.png'), (winX - 60), yRand)
+    return apple
     
 #None of my print statements are printing...
 def gameLoop():
@@ -89,18 +84,19 @@ def gameLoop():
     #Player's Score and velocities
     score = 0
     
+    leaf = Item(loadImage('Leaf.png'), winX/2, winY/2)
+    apples = []
     xVelocity = 5
     yVelocity = 5
-    leafX = 20
-    leafY = 20
-    
+   
     leafLift = 1
     leafAccel = 1
 
-
     #start the data thread
     data = GetData(mindwaveDataPointReader)
-    data.start()
+    if headset:
+        data.start()
+
     #run until done is set to true
     while not done:
         
@@ -113,14 +109,17 @@ def gameLoop():
                 if event.key == K_SPACE:
                     done = True
             
-
+        if not headset:
+            data.meditation+=random.randrange(-1.0,2.0)
+            data.attention+=random.randrange(-1.0,2.0)
+            data.poor_signal+=random.randrange(-1.0,2.0)
 
         #screen fill
         screen.fill((0,100,200))
         #screen.fill((120,120,120))
 
         #turn the screen red if there's a poor signal
-        screen.fill((200,100,100,data.poor_signal),(0,500-data.poor_signal*5/2, 800,data.poor_signal*5/2))
+        screen.fill((200,100,100),(0,500-data.poor_signal*5/2, 800,data.poor_signal*5/2))
         
 
         #leaf lift and leaf acceleration 
@@ -133,23 +132,27 @@ def gameLoop():
         xVelocity = leafAccel
         yVelocity = leafLift
         
-        leafX += xVelocity
+        leaf.x += xVelocity
         #increase X value by xVelocity
-        leafY += yVelocity
+        leaf.y += yVelocity
         
         #control values by restricting their maximum/minimum
-        if leafX >= (winX - 20):
-            leafX = (winX - 20)
-        elif leafX <= 0:
-            leafX = 0
+        if leaf.x >= (winX - 20):
+            leaf.x = (winX - 20)
+        elif leaf.x <= 0:
+            leaf.x = 0
         
-        if leafY >= winY - 20:
-            leafY = winY - 20
-        elif leafY <= 0:
-            leafY = 0
+        if leaf.y >= winY - 20:
+            leaf.y = winY - 20
+        elif leaf.y <= 0:
+            leaf.y = 0
             
-        #test creation of the image
-        loadImage('Leaf.png', leafX, leafY)
+        screen.blit(leaf.image, (leaf.x, leaf.y))
+        for apple in apples:
+            screen.blit(apple.image, (apple.x, apple.y))
+            apple.x-=1
+            if apple.x<0:
+                apples.remove(apple)
         
         #we can spawn an apple!
         #spawnApple()
@@ -161,6 +164,9 @@ def gameLoop():
 
         #add number of milliseconds since last call to score
         score += 1
+        if score%400==0:
+            apples+=[spawnApple()]        
+
         #scoreMsg is a string that should make score look prettier.
         scoreMsg = "Score: "
         #Create score font
@@ -177,10 +183,11 @@ def gameLoop():
         pygame.display.flip()
 
         clock.tick(60)
-    
-    #end the data thread
-    data.running = False
-    data.join()
+
+    if headset:
+        #end the data thread
+        data.running = False
+        data.join()
         
 
 def main():
---
+++ objects.py
@@ -0,0 +1,5 @@
+class Item:
+    def __init__(self, image, x, y):
+        self.image = image
+        self.x = x
+        self.y = y
