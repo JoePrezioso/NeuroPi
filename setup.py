2570c67f8916e8d853a6dfe980b2323ebe92425b
Valerie Magri <3lucky10@gmail.com>
2014-05-27 21:39:02 -0400

    added missing required fields

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from distutils.core import setup
 
 setup(
-    name='NeuroPi',
-    version='0.1',
-    packages=['neuropi',],
-    license='GNU GPL 3.0',
-    long_description=open('README.md').read(),
+    name = "NeuroPi",
+    version = "0.1.0",
+    author = "Valerie Magri",
+    author_email = "valerie.magri@gmail.com",
+    description = ("An experiment in reading data from a NeuroSky Mindwave into a Raspberry Pi."),
+    license = "GNU GPL 3.0",
+    keywords = "bci raspberrypi neurosky",
+    url = "https://github.com/ArcticSphinx/NeuroPi",
+    packages=['neuropi'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
 )
