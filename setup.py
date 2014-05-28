e207b96db564d73e676c365d2d491caac52c2881
Valerie Magri <3lucky10@gmail.com>
2014-05-27 21:27:05 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='NeuroPi',
+    version='0.1',
+    packages=['neuropi',],
+    license='GNU GPL 3.0',
+    long_description=open('README.md').read(),
+)
