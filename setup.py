ca4ba47ce347427f358ce143006da2097cc6544f
Valerie Magri <3lucky10@gmail.com>
2014-05-27 21:48:21 -0400

    rearranged for packaging

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-from distutils.core import setup
+import os
+from setuptools import setup
 
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 setup(
     name = "NeuroPi",
     version = "0.1.0",
@@ -11,5 +18,5 @@ setup(
     url = "https://github.com/ArcticSphinx/NeuroPi",
     packages=['neuropi'],
     long_description=read('README'),
-    classifiers=[],
+    classifiers=["Development Status :: 3 - Alpha"],
 )
